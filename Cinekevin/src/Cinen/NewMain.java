9131f0b16fd5797cf7f83f5e4613d7f346b757d2
Kevin1875 <94397214+Kevin1875@users.noreply.github.com>
2022-05-24 11:56:28 -0500

    Update NewMain.java

--- Cinekevin/src/Cinen/NewMain.java
+++ Cinekevin/src/Cinen/NewMain.java
@@ -79,24 +79,24 @@ public class NewMain {
 
 
         
-        Sala s=new Sala();
+        
+        
+         Sala s=new Sala();
         Sala s1=new Sala();
         Pelicula op=new Pelicula("nombre", "String director", 2023, 2, "String genero", "String pais", "String calificacion");
         Pelicula op2=new Pelicula("nombre", "String director", 2032, 2, "String genero", "String pais", "String calificacion");
         Cartelera cc=new Cartelera();
         
-        Cliente cl=new Cliente(1, "n" );
-        Cliente c2=new Cliente(2, "n" );
+        Cliente cl=new Cliente(1, 2,"a", "b", 3 );
+        Cliente c2=new Cliente(2, 8,"k", "c", 5 );
        
-        //System.out.println(cl.reservar(1,2,cl,s));
         
-        System.out.println(cl.reservar(2, 2, c2,s));
         
-        s.asignarSilla(1, 1, cl,2);
-        System.out.println(s.lateral_I);
-        System.out.println(s.lateral_D);
-        System.out.println(s.obtenerSillasVip());
-        System.out.println(cl.buscarCliente(2));
+        
+        
+        
+        System.out.println(Sala.admin.estadisticas(s1));
+        System.out.println(cl.comprarEntrada(cl, s1, 1, 2));
         
         
         
